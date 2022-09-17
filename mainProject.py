37252971a1d367128f047e6eaf25a6e0cd1cf623
Jack Aherne <jack.aherne03@gmail.com>
2022-09-17 12:51:41 +0100

    Update to mainProject.py
    Addition of loading Parks API's and printing the resulting elements from the object as a string

--- mainProject.py
+++ mainProject.py
@@ -1,10 +1,22 @@
 import requests
+import json
 
-url = "https://queue-times.com/parks.json"
+def runURL1(url):
+    payload = {}
+    headers = {}
+    response = requests.request("GET", url, headers=headers, data=payload)
+    data = json.loads(response.text)
+    print("Company name is = " + data[0]['name'])
+    print("Park name is = " + data[0]['parks'][0]['name'])
 
-payload={}
-headers = {}
 
-response = requests.request("GET", url, headers=headers, data=payload)
+runURL1("https://queue-times.com/parks.json")
 
-print(response.text)
+# def runURL(url):
+#     payload = {}
+#     headers = {}
+#     response = requests.request("GET", url, headers=headers, data=payload)
+#     data = json.loads(response.text)
+#     print("Wait time = " + str(data['rides'][0]['wait_time']))
+#
+# runURL("https://queue-times.com/parks/2/queue_times.json")
