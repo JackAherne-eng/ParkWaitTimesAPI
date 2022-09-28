f0c72da981c66d4f9c3bf38a09f1c4ab57cc7725
Jack Aherne <jack.aherne03@gmail.com>
2022-09-28 11:49:16 +0100

    With the use and understanding of Functions and the use of a Main, the completion of grabbing the both, wait times and the park status

--- mainProject.py
+++ mainProject.py
@@ -1,34 +1,41 @@
-import sys
-
 import requests
 import json
 
-# def runURL1(url):
-#     payload = {}
-#     headers = {}
-#     response = requests.request("GET", url, headers=headers, data=payload)
-#     data = json.loads(response.text)
-#     company = input("Enter a Company name: ")
-#     result = [x for x in data if x["name"] == company]
-#     result2 = (result[0]['parks'])
-#     print("Here you go...")
-#     for x in result2:
-#         print(x['name'])
-#
-# # Compagnie des Alpes
+
+def get_url(url):
+    response = requests.request("GET", url)
+    return response
 
 
-# runURL1("https://queue-times.com/parks.json")
-
-def runURL(url):
-    payload = {}
-    headers = {}
-    response = requests.request("GET", url, headers=headers, data=payload)
-    data = json.loads(response.text)
-
-    print(data['lands'][0]['rides'])
-
-runURL("https://queue-times.com/parks/4/queue_times.json")
+def display_companies(response):
+    for company in response.json():
+        print(company['name'], company['id'])
 
 
-# Use the ID from the Park API to input into the url for the queue times. Its a string
+def display_queue_times(response):
+    print(json.dumps(response.json(), indent=4))
+    for land in response.json()["lands"]:
+        print(land["name"])
+        for ride in land["rides"]:
+            print(ride["name"], "wait_time=", str(ride["wait_time"]), "open=", ride["is_open"])
+
+
+def main():
+    # 1) Get the companies
+    companies = get_url("https://queue-times.com/parks.json")
+
+    # 2) Display and get the companies from user
+    display_companies(companies)
+    input_company_id = 16
+
+    # 3) Get the theme parks from the user response
+    theme_parks = get_url("https://queue-times.com/parks/" + str(input_company_id) + "/queue_times.json")
+
+    # 4) Display and get the theme parks from user
+    display_queue_times(theme_parks)
+
+
+if __name__ == "__main__":
+    main()
+
+Message @ MichaelGerb2150
